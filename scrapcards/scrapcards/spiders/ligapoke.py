2f21ab9be8d930e5b08b30287aae75eba3c0b06f
LorranSilva <lorran.tito@outlook.com>
2023-03-28 15:28:52 -0300

    [FEAT]: Start project

---
+++ scrapcards/scrapcards/spiders/ligapoke.py
@@ -0,0 +1,37 @@
+import scrapy
+import re
+import json
+from scrapy_splash import SplashRequest
+
+
+class LigapokeSpider(scrapy.Spider):
+    name = 'ligapoke'
+    allowed_domains = ['www.ligapokemon.com.br']
+    start_urls = ['https://www.ligapokemon.com.br/?view=cards/home']
+
+
+    def parse(self, response):
+        collection_names = response.css('.edc-nm-2')
+        for name in collection_names:
+            next_page = {}
+            next_page['link'] = name.xpath('a/@href').extract()
+            if next_page['link'] is not None:
+                for link in next_page['link']:
+                    url = response.follow(link)
+                    yield SplashRequest(url.url, callback=self.parse_splash, args={'wait': 0.7})
+
+    def parse_splash(self, response):
+        script_text = response.xpath("//script[contains(., 'var cardsjson =')]/text()").get()
+        json_string = re.search(r"var cardsjson = (\[.*?\]);", script_text).group(1)
+        cards_data = json.loads(json_string)
+        collection_page = response.css('.tb-ed')
+        for data in collection_page:
+            yield {
+                'name': data.css('b::text').get(),
+                'acronym': data.css('.tb-ed-sigla::text').get(),
+                'cards_quantity': data.css('.tb-cards-count::text').get(),
+                'lowest_price': data.css('.tb-prc-low::text').get(),
+                'avarege_price': data.css('.tb-prc-avg::text').get(),
+                'highest_price': data.css('.tb-prc-max::text').get(),
+                'cards': cards_data
+            }
